d18999cd649c7cf0215a1d2d95b5dc3858a50259
bokholeki <bokholeki@gmail.com>
2024-12-20 23:29:53 +0700

    commit

--- db/database.js
+++ db/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
     host: 'localhost',
     port: '3306',
-    user: 'wpr',
-    password: 'fit2024',
+    user: 'root',
+    password: '123456',
     database: 'wpr2101040017'
 });
 
