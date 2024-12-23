ad2c35990e2a9843f2f614625386d2a4655b850b
bokholeki <bokholeki@gmail.com>
2024-12-23 10:55:05 +0700

    update

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
